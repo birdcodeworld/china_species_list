84082ce6017f16ccf57e8490bf08ea5d613e08ce
Geovanny <birdcode.py@gmail.com>
2025-04-15 10:52:41 -0500

    Creating fields for vernacular names in Mandarin Chinese, English, Spanish, French, German, Russian, italian, portuguese and japanese

--- project_diagnosis_species_china.sql
+++ project_diagnosis_species_china.sql
@@ -1,1 +1,9 @@
 -- Diagnosis of flora and fauna species in China
+
+--- Creating fields for vernacular names in Mandarin Chinese, English, Spanish, French, German, Russian, italian,
+--- portuguese and japanese
+
+ALTER TABLE bird_species_china ADD COLUMN name_zh varchar(80), ADD COLUMN name_en varchar(80),
+ADD COLUMN name_es varchar(80), ADD COLUMN name_fr varchar(80), ADD COLUMN name_de varchar(80), 
+ADD COLUMN name_rs varchar(80), ADD COLUMN name_it varchar(80), ADD COLUMN name_pr varchar(80),
+ADD COLUMN name_jp varchar(80);
