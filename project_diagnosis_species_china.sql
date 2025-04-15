41d49a3657e368f443a918dd1c64b2f10c3fcc8e
Geovanny <birdcode.py@gmail.com>
2025-04-15 11:31:35 -0500

    Annex of vernacular names in various languages to the list of bird species of China

--- project_diagnosis_species_china.sql
+++ project_diagnosis_species_china.sql
@@ -7,3 +7,8 @@ ALTER TABLE bird_species_china ADD COLUMN name_zh varchar(80), ADD COLUMN name_e
 ADD COLUMN name_es varchar(80), ADD COLUMN name_fr varchar(80), ADD COLUMN name_de varchar(80), 
 ADD COLUMN name_rs varchar(80), ADD COLUMN name_it varchar(80), ADD COLUMN name_pr varchar(80),
 ADD COLUMN name_jp varchar(80);
+
+--- Annex of vernacular names in various languages to the list of bird species of China
+
+UPDATE bird_species_china SET name_jp = multilingual_name_list.japanese
+FROM multilingual_name_list WHERE bird_species_china.species = multilingual_name_list.species;
